0c9570fa477744f2a42167cba2e38fe2c94d8cf2
Vaishnavi Srinivasan <Vaishnavi_Srinivasan@intuit.com>
2023-03-17 09:40:56 +0530

    gitignore

---
+++ src/main/java/com.org.ridemanage/RideManagementApp.java
@@ -0,0 +1,11 @@
+package com.org.ridemanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RideManagementApp {
+    public static void main(String[] args) {
+        SpringApplication.run(RideManagementApp.class, args);
+    }
+}
